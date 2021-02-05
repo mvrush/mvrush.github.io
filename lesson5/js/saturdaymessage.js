dd43ae76ee1d2c0aa80c26cddf0bae00f5d25558
mvrush <mvrush@gmail.com>
2021-02-04 22:15:42 -0700

    JavaScript add

---
+++ lesson5/js/saturdaymessage.js
@@ -0,0 +1,11 @@
+// displays the message on saturday only
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getDay();
+console.log(dayNumber);
+
+const element = document.getElementById("message");
+if (dayNumber == 6) {
+    element.classList.add("showme");
+}  else element.classList.add("hideme");
--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -2,10 +2,3 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide"); 
     /* what this does is toggle the class "hide" on and off in the html */
 }
-
-//Today's date display in footer
-const save_date = new Date();
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-var today = days[save_date.getDay()] + ", " + save_date.getDate() + " " + months[save_date.getMonth()] + " " + save_date.getFullYear()
-document.getElementById("updatetime").textContent = today;
