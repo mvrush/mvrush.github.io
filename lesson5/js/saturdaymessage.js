fe1c2f2c9e05932a2dfe2df0fb8698c1ff4ff728
mvrush <mvrush@gmail.com>
2021-02-05 19:46:56 -0700

    Preston Updates

--- lesson5/js/saturdaymessage.js
+++ lesson5/js/saturdaymessage.js
@@ -6,6 +6,6 @@ const dayNumber = today.getDay();
 console.log(dayNumber);
 
 const element = document.getElementById("message");
-if (dayNumber == 6) {
+if (dayNumber == 5) {
     element.classList.add("showme");
 }  else element.classList.add("hideme");
