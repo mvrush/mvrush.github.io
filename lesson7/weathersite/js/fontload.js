a162c2e13536f807f87ccabe4feb8e4fbd452ac9
mvrush <mvrush@gmail.com>
2021-02-23 14:51:30 -0700

    Update JS to load font weights

--- lesson7/weathersite/js/fontload.js
+++ lesson7/weathersite/js/fontload.js
@@ -2,8 +2,8 @@
 WebFont.load({
     google: {
         families: [
-            "Lato",
-            "Alegreya"
+            "Lato:400,500,700,800,900",
+            "Alegreya:400,500,700,800,900"
         ]
     }
 });
