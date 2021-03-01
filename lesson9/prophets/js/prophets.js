bbe720345bd33761b861639e2bfe815284e13f0d
mvrush <mvrush@gmail.com>
2021-03-01 16:23:42 -0700

    prophets update

--- lesson9/prophets/js/prophets.js
+++ lesson9/prophets/js/prophets.js
@@ -5,5 +5,15 @@ fetch(requestURL)
 })
 .then(function (jsonObject) {
     console.table(jsonObject); //temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];  //stores the results of the converted response into an array
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+        card.appendChild(h2);
+
+        document.querySelector('div.cards').appendChild(card);
+    }
 });
-const prophets = jsonObject['prophets'];  //stores the results of the converted response into an array
