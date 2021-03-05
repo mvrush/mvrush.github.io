6ad007cde6c0a5d2b105417682afda1260180f58
mvrush <mvrush@gmail.com>
2021-03-04 21:53:24 -0700

    Prophets

--- lesson9/prophets/prophets_from_lab.js
+++ lesson9/prophets/prophets_from_lab.js
@@ -6,11 +6,14 @@ fetch(requestURL)
 .then(function (jsonObject) {
     console.table(jsonObject); //temporary checking for valid response and data parsing remove this before submitting assignment
     const prophets = jsonObject['prophets'];  //stores the results of the converted response into an array
-    for (let i = 0; i < prophets.length; i++ ) {
+    prophets.forEach(prophet => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        card.appendChild(h2);
+        // use template literals
+        h2.innerHTML = `${prophet.name} ${prophet.lastname}`
+        card.append(h2);
+        cards.append(card);
+    });
 
         //appends birthdate
         let birthdate = document.createElement('p');
