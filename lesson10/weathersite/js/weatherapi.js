16918ee846deba7903d78bad6f4a2343afeb9e75
mvrush <mvrush@gmail.com>
2021-03-10 23:05:41 -0700

    Current Conditions done!

---
+++ lesson10/weathersite/js/weatherapi.js
@@ -0,0 +1,26 @@
+//This is the Current Weather Conditions Code
+//ADD the key and change units to imperial
+const apiURL = "//api.openweathermap.org/data/2.5/weather?zip=83263,us&appid=c0fc88d8632588356324367e7fd1e57a&units=imperial";
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+    
+    document.getElementById('currconditions').innerHTML=Math.round(weatherInfo.main.temp);
+    document.getElementById('skies').innerHTML=weatherInfo.weather[0].description;
+    document.getElementById('hightemp').innerHTML=Math.round(weatherInfo.main.temp_max);
+    document.getElementById('lowtemp').innerHTML=Math.round(weatherInfo.main.temp_min);
+    document.getElementById('speed').innerHTML=Math.round(weatherInfo.wind.speed);
+    document.getElementById('chill').innerHTML=Math.round(weatherInfo.main.feels_like);
+
+    const iconcode = weatherInfo.weather[0].icon;
+    console.log(iconcode);
+    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    console.log(icon_path);
+
+    document.getElementById('weather_icon').src = icon_path;
+
+ }); //end of "then" fat arrow function
