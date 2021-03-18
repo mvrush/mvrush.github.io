190cdfd0d61c09cf9a7dd6ba7251794b90926542
mvrush <mvrush@gmail.com>
2021-03-17 22:20:32 -0600

    Article Update

--- lesson11/js/weatherapi_sodasprings.js
+++ lesson11/js/weatherapi_sodasprings.js
@@ -90,3 +90,32 @@ fetch(apiURL)
  
  });
  
+   // Town Events for Fish Haven
+   const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+   fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+      .then (function (jsonObject) {
+        const towns = jsonObject["towns"];
+        const event = document.querySelector(".events"); //writes the events to the div in document with id=events
+        console.log(towns);
+  
+        const townfilter = towns.filter((town) => town.name == "Soda Springs");
+        townfilter.forEach((town) => {
+          let title = document.createElement("h2");
+          let eventadd = document.createElement("div");
+  
+          title.innerHTML = town.name + ` Events`; //writes the h2 title
+  
+          let myevents = town.events;
+          for (i = 0; i < myevents.length; i++) {
+            let activity = document.createElement("li");
+            activity.innerHTML = `${town.events[i]}`
+            eventadd.append(activity);
+          };
+          event.append(title);
+          event.append(eventadd);
+        });
+      });
