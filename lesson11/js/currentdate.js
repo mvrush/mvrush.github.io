135cfc417bd21cca573ad6e6c17edd5fd1310129
mvrush <mvrush@gmail.com>
2021-03-20 19:35:33 -0600

    Lesson 11 Reset 1

--- lesson11/js/currentdate.js
+++
@@ -1,6 +0,0 @@
-//Today's date display in footer
-const save_date = new Date();
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-var todayDate = days[save_date.getDay()] + ", " + save_date.getDate() + " " + months[save_date.getMonth()] + " " + save_date.getFullYear()
-document.getElementById("updatetime").textContent = todayDate;
