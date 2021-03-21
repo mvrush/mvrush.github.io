43590f132b40477bb4d2ae557208266dbfd09b1d
mvrush <mvrush@gmail.com>
2021-03-20 19:36:41 -0600

    Lesson11 Reset 2

---
+++ lesson11/js/fontload.js
@@ -0,0 +1,9 @@
+// Loads Google fonts and works in tandem with the Google webfont.js found on the html page
+WebFont.load({
+    google: {
+        families: [
+            "Lato:400,500,700,800,900",
+            "Alegreya:400,500,700,800,900"
+        ]
+    }
+});
