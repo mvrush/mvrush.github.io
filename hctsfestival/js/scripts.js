d9d0c1e67f1b643c95ab0db0a02b781e97c33116
mvrush <mvrush@gmail.com>
2021-04-14 22:53:14 -0600

    HCTS Page

---
+++ hctsfestival/js/scripts.js
@@ -0,0 +1,35 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide"); 
+}
+
+
+const save_date = new Date();
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var todayDate = days[save_date.getDay()] + ", " + save_date.getDate() + " " + months[save_date.getMonth()] + " " + save_date.getFullYear()
+document.getElementById("updatetime").textContent = todayDate;
+
+
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
+
+
+    function listView() {
+        document.getElementById("directory").classList.add("list-view");
+    }
+
+    function gridView() {
+        document.getElementById("directory").classList.remove("list-view");
+    }
+
+
+function removeAlert() {          
+    document.getElementById("close").classList.add("disabled");
+  } 
+
+
