d494040690f82ff70d1a9716695b436844fe06cd
mvrush <mvrush@gmail.com>
2021-04-26 19:33:48 -0600

    Update Nav Menu and JS

--- hctsfestival/js/scripts.js
+++ hctsfestival/js/scripts.js
@@ -1,5 +1,6 @@
 function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("hide"); 
+    document.getElementById("primaryNav1").classList.toggle("hide");
+    document.getElementById("primaryNav2").classList.toggle("hide");
 }
 
 
