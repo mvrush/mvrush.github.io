c1fee6883d2f10249c629b5826b5284364f54728
mvrush <mvrush@gmail.com>
2021-05-10 16:26:17 -0600

    Sticky Nav

--- hctsfestival/js/scripts.js
+++ hctsfestival/js/scripts.js
@@ -1,9 +1,26 @@
+//hamburger toggle
 function toggleMenu() {
-    document.getElementById("primaryNav1").classList.toggle("hide");
-    document.getElementById("primaryNav2").classList.toggle("hide");
+    document.getElementById("primaryNav").classList.toggle("hide");
 }
 
+/*---------STICKY NAVBAR------------*/
+//When user scrolls the page, execute stickyScroll
+window.onscroll = function() {myFunction()};
+//Get the navbar
+var navbar = document.getElementById("navbar");
+//Get the offest position of the navbar
+var sticky = navbar.offsetTop;
+//Add the sticky class to the navbar when you reach it's scroll position. Remove "sticky" when you leave scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky")
+    }
+    else {
+        navbar.classList.remove("sticky");
+    }
+}
 
+//Current Date in Footer
 const save_date = new Date();
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -11,6 +28,7 @@ var todayDate = days[save_date.getDay()] + ", " + save_date.getDate() + " " + mo
 document.getElementById("updatetime").textContent = todayDate;
 
 
+//following code is from weather site
 function selectResponse() {
 	const s = document.querySelector('#selected')
 	const sel = document.querySelector('#selectbrowser');
