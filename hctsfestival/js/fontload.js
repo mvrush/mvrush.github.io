f4a2d52769329950d9e96c01e5b2e824bb8a6f2d
mvrush <mvrush@gmail.com>
2021-05-12 17:06:19 -0600

    Remove hctsfestival folder

--- hctsfestival/js/fontload.js
+++
@@ -1,8 +0,0 @@
-WebFont.load({
-    google: {
-        families: [
-            "Cormorant Garamond:0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700",
-            "Fira sans:400,500,600,700,800,900"
-        ]
-    }
-});
